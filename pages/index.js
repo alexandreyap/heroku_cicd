6ef3cd2c83bd2e7bce10e3a4be87ba9ece447583
alexandreyap <alexandre.yap@epita.fr>
2022-05-16 15:42:02 +0200

    modify index

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! My name is Yappy</a>
         </h1>
 
         <p className={styles.description}>
