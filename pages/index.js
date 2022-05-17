647211d7c4cd56c98bdcc67c65d309ab366132ce
alexandreyap <alexandre.yap@epita.fr>
2022-05-17 11:33:21 +0200

    hugogo

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://cri.epita.fr/users/hugo.bringout/">My name is Hugo bringout</a>
+          Welcome to <a href="https://cri.epita.fr/users/hugo.bringout/">My name is Hugogo</a>
         </h1>
 
         <p className={styles.description}>
